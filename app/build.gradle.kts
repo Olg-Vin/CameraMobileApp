8513996e99be9e0fd80d9b453e273068d8487e9c
Vinio <Ewq@1605>
2025-01-13 00:55:57 +0300

    [ADD] add dependencies for cameraX and on view binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,21 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+//    Camera
+    var camerax_version = "1.2.2"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-video:${camerax_version}")
+
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation("androidx.camera:camera-extensions:${camerax_version}")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
